0814cf70823785e787d6548d22a87f57a9457b29
Istvan Zoltan Ballok <istvan.zoltan.ballok@sap.com>
2023-01-13 12:33:37 +0100

    Remove the Grafana->Plutono, Loki->Vali replacement scripts
    These scripts were used to generate the preceding commits.
    
    Co-authored-by: Istvan Zoltan Ballok <istvan.zoltan.ballok@sap.com>
    Co-authored-by: Jeremy Rickards <jeremy.rickards@sap.com>

--- .scripts/steps/2-grafana.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Replace the Grafana container image with that of Plutono
-
-git grep -z -l "repository: grafana/grafana" -- ':!/vendor' ':!/.scripts' ':!NOTICE.md' \
-| xargs -0 sed -i -E 's|repository: grafana/grafana|repository: ghcr.io/credativ/plutono|
-                      s/tag: "7.5.17"/tag: "main" # TODO/'
--- .scripts/steps/2-loki.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Replace the Loki container image with that of Vali
-
-git grep -z -l "repository: grafana/loki" -- ':!/vendor' ':!/.scripts' ':!NOTICE.md' \
-| xargs -0 sed -i -E 's|repository: grafana/loki|repository: ghcr.io/credativ/vali|
-                      s|repository: "docker.io/grafana/promtail"|repository: "ghcr.io/credativ/valitail"|
-                      s/tag: "2.2.1"/tag: "main" # TODO/'
