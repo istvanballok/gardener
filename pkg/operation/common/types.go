521cc28c3153791ce1a7f4cebe2f4347d0dc6de3
Istvan Zoltan Ballok <istvan.zoltan.ballok@sap.com>
2023-03-31 12:54:36 +0200

    15. Replace Grafana with Plutono in file contents
    git grep -z -l Grafana -- ':!/vendor' ':!/.scripts' ':!NOTICE.md' \
    | xargs -0 sed -i 's/Grafana/Plutono/g'

--- pkg/operation/common/types.go
+++ pkg/operation/common/types.go
@@ -18,8 +18,8 @@ const (
 	// VPNTunnel dictates that VPN is used as a tunnel between seed and shoot networks.
 	VPNTunnel string = "vpn-shoot"
 
-	// GrafanaUsersPrefix is a constant for a prefix used for the users Grafana instance.
-	GrafanaUsersPrefix = "gu"
+	// PlutonoUsersPrefix is a constant for a prefix used for the users Plutono instance.
+	PlutonoUsersPrefix = "gu"
 
 	// PrometheusPrefix is a constant for a prefix used for the Prometheus instance.
 	PrometheusPrefix = "p"
