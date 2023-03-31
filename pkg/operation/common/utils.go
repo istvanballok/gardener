1e4975a37a26e9aa8805fe177c01de2ad8ac3739
Istvan Zoltan Ballok <istvan.zoltan.ballok@sap.com>
2023-03-31 12:54:31 +0200

    12. Replace grafana with plutono in file contents
    git grep -z -l grafana -- ':!/vendor' ':!/.scripts' ':!NOTICE.md' \
    | xargs -0 sed -i 's/grafana/plutono/g'

--- pkg/operation/common/utils.go
+++ pkg/operation/common/utils.go
@@ -190,7 +190,7 @@ func DeleteGrafana(ctx context.Context, k8sClient kubernetes.Interface, namespac
 	deleteOptions := []client.DeleteAllOfOption{
 		client.InNamespace(namespace),
 		client.MatchingLabels{
-			"component": "grafana",
+			"component": "plutono",
 		},
 	}
 
@@ -214,7 +214,7 @@ func DeleteGrafana(ctx context.Context, k8sClient kubernetes.Interface, namespac
 		ctx,
 		&corev1.Service{
 			ObjectMeta: metav1.ObjectMeta{
-				Name:      "grafana",
+				Name:      "plutono",
 				Namespace: namespace,
 			}},
 	); client.IgnoreNotFound(err) != nil {
