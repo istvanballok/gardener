17faedacd12fc5688664b80954b62b0b296201a5
Istvan Zoltan Ballok <istvan.zoltan.ballok@sap.com>
2023-04-04 13:14:51 +0200

    Rename the loki folder on loki's disk to vali
    Co-authored-by: Istvan Zoltan Ballok <istvan.zoltan.ballok@sap.com>
    Co-authored-by: Jeremy Rickards <jeremy.rickards@sap.com>
    Co-authored-by: Shafeeque E S <shafeeque.e.s@sap.com>

--- pkg/operation/botanist/logging.go
+++ pkg/operation/botanist/logging.go
@@ -61,6 +61,7 @@ func (b *Botanist) DeploySeedLogging(ctx context.Context) error {
 		images.ImageNameKubeRbacProxy,
 		images.ImageNameTelegraf,
 		images.ImageNameTune2fs,
+		images.ImageNameAlpine,
 	)
 	if err != nil {
 		return err
