c32bdd74f456ba38e2c90831a4db99ddcafbe9a5
Istvan Zoltan Ballok <istvan.zoltan.ballok@sap.com>
2023-04-19 14:11:22 +0200

    1. Replace the Grafana Github page with that of Plutono
    git grep -z -l github\\.com/grafana/grafana -- ':!/vendor' ':!/.scripts' ':!NOTICE.md' \
    | xargs -0 sed -i -E 's|github.com/grafana/grafana|github.com/credativ/plutono|g'

--- .scripts/steps/2-grafana.sh
+++ .scripts/steps/2-grafana.sh
@@ -7,4 +7,4 @@
 
 git grep -z -l "repository: eu.gcr.io/gardener-project/3rd/grafana/grafana" -- ':!/vendor' ':!/.scripts' ':!NOTICE.md' \
 | xargs -0 sed -i -E 's|repository: eu.gcr.io/gardener-project/3rd/grafana/grafana|repository: ghcr.io/credativ/plutono|
-                      s/tag: "7.5.17"/tag: "v7.5.20"/'
+                      s/tag: "7.5.17"/tag: "v7.5.21"/'
--- .scripts/steps/2-loki.sh
+++ .scripts/steps/2-loki.sh
@@ -8,4 +8,4 @@
 git grep -z -l "repository: eu.gcr.io/gardener-project/3rd/grafana/loki" -- ':!/vendor' ':!/.scripts' ':!NOTICE.md' \
 | xargs -0 sed -i -E 's|repository: eu.gcr.io/gardener-project/3rd/grafana/loki|repository: ghcr.io/credativ/vali|
                       s|repository: eu.gcr.io/gardener-project/3rd/grafana/promtail|repository: ghcr.io/credativ/valitail|
-                      s/tag: "2.2.1"/tag: "v2.2.4"/'
+                      s/tag: "2.2.1"/tag: "v2.2.5"/'
