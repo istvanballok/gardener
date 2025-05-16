43ebc7e8ae729a26b20b01d8a4f1d776ddd98565
Istvan Zoltan Ballok <istvan.zoltan.ballok@sap.com>
2025-05-16 10:51:20 +0200

    Show the VPA recommendations dashboard for the seeds
    VPA is required for the seed clusters:
    
    pkg/gardenlet/controller/seed/seed/reconciler_reconcile.go:
    ...
    func (r *Reconciler) runReconcileSeedFlow(
    	ctx context.Context,
    	log logr.Logger,
    	seed *seedpkg.Seed,
    	seedIsGarden bool,
    	seedIsShoot bool,
    ) error {
    	// VPA is a prerequisite. If it's enabled then we deploy the CRD (and later also the related components) as part of
    	// the flow. However, when it's disabled then we check whether it is indeed available (and fail, otherwise).
    	if !vpaEnabled(seed.GetInfo().Spec.Settings) {
    		if _, err := r.SeedClientSet.Client().RESTMapper().RESTMapping(schema.GroupKind{Group: "autoscaling.k8s.io", Kind: "VerticalPodAutoscaler"}); err != nil {
    			return fmt.Errorf("VPA is required for seed cluster: %s", err)
    		}
    	}
    
    So we should configure the Plutono component for the seeds in such a
    way that it deploys the VPA dashboards.

--- pkg/gardenlet/controller/seed/seed/components.go
+++ pkg/gardenlet/controller/seed/seed/components.go
@@ -543,7 +543,7 @@ func (r *Reconciler) newPlutono(seed *seedpkg.Seed, secretsManager secretsmanage
 		false,
 		false,
 		false,
-		v1beta1helper.SeedSettingVerticalPodAutoscalerEnabled(seed.GetInfo().Spec.Settings),
+		true,
 		wildcardCertName,
 	)
 }
