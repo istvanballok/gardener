deabff3e1a9ed2b7b8230e399505a4d923caedd2
Istvan Zoltan Ballok <istvan.zoltan.ballok@sap.com>
2026-01-27 16:01:27 +0100

    Adopt managed seed integration test: TODO
    Co-authored-by: Victor Herrero Otal <victor.herrero.otal@sap.com>
    Co-authored-by: Istvan Zoltan Ballok <istvan.zoltan.ballok@sap.com>

--- test/integration/controllermanager/managedseedset/managedseedset_test.go
+++ test/integration/controllermanager/managedseedset/managedseedset_test.go
@@ -92,6 +92,7 @@ var _ = Describe("ManagedSeedSet controller test", func() {
 				Conditions: []gardencorev1beta1.Condition{
 					{Type: gardencorev1beta1.GardenletReady, Status: gardencorev1beta1.ConditionTrue},
 					{Type: gardencorev1beta1.SeedSystemComponentsHealthy, Status: gardencorev1beta1.ConditionTrue},
+					{Type: gardencorev1beta1.SeedObservabilityComponentsHealthy, Status: gardencorev1beta1.ConditionTrue},
 					{Type: gardencorev1beta1.SeedBackupBucketsReady, Status: gardencorev1beta1.ConditionTrue},
 				},
 			}
